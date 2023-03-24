b476dd94095a4e57239c07e4a8ef0d9c63388c0c
DeKayla Phillips <dphillips@8thlight.com>
2023-03-24 16:09:48 -0400

    Refactored game class for ES6 syntax usage

--- Game.js
+++ Game.js
@@ -1,18 +1,36 @@
-module.exports = {
-    displaySingleRow: () => { return "  |  |  " },
-    
-    displayThreeRows: () => { 
-        let row = "  |  |  ";
-        const allRows = `${row}\n${row}\n${row}`;
-        return allRows; 
-    },
+export class Game {
+    constructor(rows=3) {
+        this.rowTotal = rows
+        this.row = "    |   |    "
+        this.line = "- - - - - - -"
+    }
+    /* 
+       CODE LAYERS 
+       Basic idea: Seperate the Responsibility 
+       SOLID
+       S - Separate the Responsibility 
+            Each layer should do one thing
 
-    displaySeparatedRows: () => {
-        let row = "    |   |    "
-        let dashes = "- - - - - - -"
-        const board = `${row}\n${dashes}\n${row}\n${dashes}\n${row}`
-        return board
-    }  
+       O - Open to Extension
+            A function or class/general data structure is easy to change    
+
+    */
+    getRows(divider) {
+        const lineSeparator = (divider === undefined) ? `${this.line}\n` : divider
+        const arrayOfRows = new Array(this.rowTotal).fill(`${this.row}\n`)
+        return arrayOfRows.join(lineSeparator)
+    }
 }
 
 
+// Uncomment the code below to display a single row of the Tic-Tac-Toe board.
+// const game = new TicTacToe(1)
+// console.log(game.displayRows())
+
+// Uncomment the code below to display three rows of the Tic-Tac-Toe board.
+// const game = new TicTacToe(3)
+// console.log(game.displayRows())
+
+// Uncomment the code below to display the rows separates by dashes.
+// const game = new TicTacToe()
+// console.log(game.displayBoard())
--- game.test.js
+++ game.test.js
@@ -1,34 +1,39 @@
 // To run tests --> $ npm test
 
-const ticTacToeObject = require('./game')
+import { Game } from './game.js'
 
 describe('display a single row', () => {
     test('when the displaySingleRow function is called, the console will display a single row correctly', () => {
-        const {displaySingleRow: displayRow} = ticTacToeObject;
-        const singleRow = displayRow();
-        const row = "  |  |  "
+        // ES6 Syntax
+        const game = new Game(1)
+        const row = "    |   |    \n"
 
-        expect(singleRow).toBe(row); 
+        const displaySingleRow = game.getRows("")
+
+        expect(displaySingleRow).toBe(row)  
     });
 });
 
 describe('display all three rows', () => {
     test('when the displayThreeRows function is called, the console will display three stacked rows correctly', () => {
-        const {displayThreeRows: displayRows} = ticTacToeObject;
-        const threeRows = displayRows();
-        const rows = "  |  |  \n  |  |  \n  |  |  ";
-
+        const game = new Game();
+        const row = "    |   |    \n"
+        const rows =  `${row}${row}${row}`
+        
+        const threeRows = game.getRows("");
+        
         expect(threeRows).toBe(rows)
     })
 }) 
 
 describe('display rows separated by dashes', () => {
     test('when the displaySeparatedRows function is called, the console will display three stacked rows separated by dashes correctly', () => {
-        const {displaySeparatedRows: displayBoard} = ticTacToeObject;
-        const rowsWithDashes = displayBoard();
-        const row = "    |   |    "
-        const dashes = "- - - - - - -"
-        const board = `${row}\n${dashes}\n${row}\n${dashes}\n${row}`
+        const game = new Game()
+        const row = "    |   |    \n"
+        const dashes = "- - - - - - -\n"
+        const board = `${row}${dashes}${row}${dashes}${row}`
+
+        const rowsWithDashes = game.getRows();
             
         expect(rowsWithDashes).toBe(board)
     })
