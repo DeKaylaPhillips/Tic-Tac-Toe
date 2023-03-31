142bf51589e35956cd954fa42bf7500457c4a8d0
DeKayla Phillips <dphillips@8thlight.com>
2023-03-31 17:24:04 -0400

    Create method to get board
    Method created to return and display a board to the console.

--- Game.js
+++ Game.js
@@ -3,6 +3,8 @@ export class Game {
         this.rowTotal = rows
         this.row = "    |   |    "
         this.line = "- - - - - - -"
+        this.cells = new Array(9).fill('')
+        this.board = `  ${this.cells[0]}  |  ${this.cells[1]}  |  ${this.cells[2]}  \n${this.line}\n  ${this.cells[3]}  |  ${this.cells[4]}  |  ${this.cells[5]}  \n${this.line}\n  ${this.cells[6]}  |  ${this.cells[7]}  |  ${this.cells[8]}  `
     }
 
     getRows(divider) {
@@ -10,5 +12,11 @@ export class Game {
         const arrayOfRows = new Array(this.rowTotal).fill(`${this.row}\n`)
         return arrayOfRows.join(lineSeparator)
     }
+
+    getBoard() {
+       return this.board
+    }
 }
+
+
 
