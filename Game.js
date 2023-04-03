f240531e01d396ede6f465a580f8ba315bcc1840
DeKayla Phillips <dphillips@8thlight.com>
2023-04-03 13:13:27 -0400

    Rename class and method
    'getRows()' function renamed to 'printBoard()' to convey logic of the class method. Class 'Game' renamed to 'Board' to convey usage and manage responsibilities.

--- Game.js
+++ Game.js
@@ -1,22 +1,19 @@
-export class Game {
+export class Board {
     constructor(rows=3) {
         this.rowTotal = rows
         this.row = "    |   |    "
         this.line = "- - - - - - -"
         this.cells = new Array(9).fill('')
-        this.board = `  ${this.cells[0]}  |  ${this.cells[1]}  |  ${this.cells[2]}  \n${this.line}\n  ${this.cells[3]}  |  ${this.cells[4]}  |  ${this.cells[5]}  \n${this.line}\n  ${this.cells[6]}  |  ${this.cells[7]}  |  ${this.cells[8]}  `
     }
 
-    getRows(divider) {
+    getRows(divider) { // business logic
         const lineSeparator = (divider === undefined) ? `${this.line}\n` : divider
         const arrayOfRows = new Array(this.rowTotal).fill(`${this.row}\n`)
         return arrayOfRows.join(lineSeparator)
     }
 
-    getBoard() {
-       return this.board
+    printBoard() { // printing logic
+       return `  ${this.cells[0]}  |  ${this.cells[1]}  |  ${this.cells[2]}  \n${this.line}\n  ${this.cells[3]}  |  ${this.cells[4]}  |  ${this.cells[5]}  \n${this.line}\n  ${this.cells[6]}  |  ${this.cells[7]}  |  ${this.cells[8]}  `
     }
 }
-
-
 
