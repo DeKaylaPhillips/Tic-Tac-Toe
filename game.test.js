d8b0ebadb9f6d09be0ef76e4ca93810b8fba0dd9
DeKayla Phillips <dphillips@8thlight.com>
2023-04-03 13:17:48 -0400

    Refactored test file
    Renamed import 'Game' to 'Board' and updated tests to instantiate the renamed class 'Board' and changed variable names as needed.

--- game.test.js
+++ game.test.js
@@ -1,13 +1,13 @@
 // To run tests --> $ npm test
 
-import { Game } from './Game'
+import { Board } from './Game'
 
 describe('display a single row', () => {
     test('when the displaySingleRow function is called, the console will display a single row correctly', () => {
-        const game = new Game(1)
+        const board = new Board(1)
         const row = "    |   |    \n"
 
-        const displaySingleRow = game.getRows("")
+        const displaySingleRow = board.getRows("")
 
         expect(displaySingleRow).toBe(row)  
     });
@@ -15,11 +15,11 @@ describe('display a single row', () => {
 
 describe('display all three rows', () => {
     test('when the threeRows function is called, the console will display three stacked rows correctly', () => {
-        const game = new Game();
+        const board = new Board();
         const row = "    |   |    \n"
         const rows =  `${row}${row}${row}`
         
-        const threeRows = game.getRows("");
+        const threeRows = board.getRows("");
         
         expect(threeRows).toBe(rows)
     });
@@ -27,27 +27,27 @@ describe('display all three rows', () => {
 
 describe('display rows separated by dashes', () => {
     test('when the rowsWithDashes function is called, the console will display three stacked rows separated by dashes correctly', () => {
-        const game = new Game()
+        const board = new Board()
         const row = "    |   |    \n"
         const dashes = "- - - - - - -\n"
-        const board = `${row}${dashes}${row}${dashes}${row}`
+        const rows_dashes = `${row}${dashes}${row}${dashes}${row}`
 
-        const rowsWithDashes = game.getRows();
+        const rowsWithDashes = board.getRows();
             
-        expect(rowsWithDashes).toBe(board)
+        expect(rowsWithDashes).toBe(rows_dashes)
     });
 });
 
 describe('tic-tac-toe board', () => {
     test("has a structure that can accept and store values", () => {
-        const game = new Game()
+        const board = new Board()
         const cells = new Array(9).fill('')
-        const line = game.line
+        const line = board.line
         const expectedBoard = `  ${cells[0]}  |  ${cells[1]}  |  ${cells[2]}  \n${line}\n  ${cells[3]}  |  ${cells[4]}  |  ${cells[5]}  \n${line}\n  ${cells[6]}  |  ${cells[7]}  |  ${cells[8]}  `
 
-        const board = game.getBoard();
+        const displayBoard = board.printBoard();
 
-        expect(board).toStrictEqual(expectedBoard);
+        expect(displayBoard).toStrictEqual(expectedBoard);
     });
 });
 
