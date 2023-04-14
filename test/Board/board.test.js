ffbb27218c14f0923914a8d8aa528860a715f3ce
DeKayla Phillips <dphillips@8thlight.com>
2023-04-14 16:08:41 -0400

    Refactor board.js implementations and tests
    Board class refactored to only manage the state of the Tic-Tac-Toe board, accept a given number of columns, have a 'cells' property that initializes a new nested array of rows and columns, and provide information on the total number of columns passed to the class. getRows() method removed. printBoard() method removed. Logic of these two methods now persists in newly created Display class. Display class responsible for managing the format/construction of the board based on the instance of the board state object passed to it. The line  property of pre-existing Board class moved to Display class, refactored to use the repeat() method to create a dashed line, and renamed as the rowSeparator.
    
    Board tests refactored to fit the refactored implementations of the Board Class and Display Class. All tests passing and in working state. Refactored board assembly method in progress.

--- test/Board/board.test.js
+++ test/Board/board.test.js
@@ -1,18 +1,79 @@
 // To run tests --> $ npm test
+import { Board, Display } from '../../lib/Board/board'
 
-import { Board } from '../../lib/Board/board'
+describe('Board', () => {
+    test('will return a structure storing each individual cell in the board when getCells() is called', () => {
+        const board = new Board()
+        const expectedResults = board.cells
 
-describe('display a single row', () => {
-    test('when the displaySingleRow function is called, the console will display a single row correctly', () => {
-        const board = new Board(1)
-        const row = "    |   |    \n"
-
-        const displaySingleRow = board.getRows("")
-
-        expect(displaySingleRow).toBe(row)  
+        const cellStructure = board.getCells()
+        
+        expect(cellStructure).toBe(expectedResults)
     });
 });
 
+describe('Display', () => {
+    test('will format a cell without a column separator into a column when formatCells() is called', () => {
+        const board = new Board(1, 3)
+        const display = new Display(board)
+        const expectedResults = ` `;
+
+        const formattedCellWithSeparator = display.formatCells(board.cells[0][2], 2)
+
+        expect(formattedCellWithSeparator).toBe(expectedResults)
+
+    });
+
+    test('will format a cell with a column separator into a column when formatCells() is called', () => {
+        const board = new Board(1, 3)
+        const display = new Display(board)
+        const expectedResults = `   |`;
+
+        const formattedCellNoSeparator = display.formatCells(board.cells[0][0], 0)
+
+        expect(formattedCellNoSeparator).toBe(expectedResults)
+
+    });
+
+    test('will print a single row to the console when an instance of the Board is passed to the class where 1 is an arg for rows', () => {
+        const board = new Board(1, 3)
+        const display = new Display(board)
+        const expectedResults = '   |    |  '
+
+        const row = display.getBoard()
+        
+        expect(row).toBe(expectedResults)
+    });
+
+    test('will print a Tic-Tac-Toe board to the console when an instance of the Board is passed to the class', () => {
+        const board = new Board(3, 3)
+        const display = new Display(board)
+        const expectedResults = display.boardDisplay
+
+        const printedBoard = display.getBoard()
+        
+        expect(printedBoard).toBe(expectedResults)
+    });
+
+    // test('will print 3 stacked rows to the console when an instance of the Board is passed to the class where 3 is an arg for rows', () => {
+        // TO-DO
+    // });
+
+});
+
+
+// describe('BoardAssembly', () => {
+//     test('will display a single row when an instance of the BoardAssembly is created with 1 row and the getRows() method is called', () => {
+//         const board = new BoardAssembly(1)
+//         const row = ` ${this.col} | ${this.col}  |  ${this.col}  \n`
+
+//         const displaySingleRow = board.getRows("")
+
+//         expect(displaySingleRow).toBe(row)  
+//     });
+// });
+
+/*
 describe('display all three rows', () => {
     test('when the threeRows function is called, the console will display three stacked rows correctly', () => {
         const board = new Board();
@@ -50,4 +111,4 @@ describe('tic-tac-toe board', () => {
         expect(displayBoard).toStrictEqual(expectedBoard);
     });
 });
-
+*/
