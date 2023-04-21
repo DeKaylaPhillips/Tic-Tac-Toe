ba4976ccbb97c44b4a226e927565bf34fca8f4f2
DeKayla Phillips <dphillips@8thlight.com>
2023-04-21 17:05:33 -0400

    Delete display test suite file

--- test/Display/display.test.js
+++
@@ -1,9 +0,0 @@
-import { Display } from "../../lib/Display/display";
-import { Board } from "../../lib/Board/board";
-
-describe('Display', () => {
-    test("foo", () => {
-expect(true).toBe(true)
-    })
-});
-
