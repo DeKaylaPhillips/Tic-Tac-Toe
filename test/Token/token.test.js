308d5bedbd113670db6d6f4d9e09422f3464d4df
DeKayla Phillips <dphillips@8thlight.com>
2023-04-25 17:03:44 -0400

    Refactor constructor test; test passes with current implementation

--- test/Token/token.test.js
+++ test/Token/token.test.js
@@ -3,22 +3,18 @@ import { Token } from '../../lib/Token/token';
 describe('Token Class', () => {
 
     describe('Token constructor', () => {
-        test('will accept a string to represent a player\'s token', () => {
+        test('will accept a token that is a string', () => {
             const token = new Token('X')
-            const playerToken = token.player
-            expect(playerToken).toBe('X')
+            const tokenValue = token.player
+            expect(typeof tokenValue).toBe('string')
         });  
 
-        test('will accept a token that is a string', () => {
-
-        }); 
-
         test('will accept a token that is a length of 1', () => {
-
+            // test that the token has a length of 1
         });
 
         test('will return a validated token', () => {
-
+            // test that the proper token value is returned if return
         });
     });
 });
