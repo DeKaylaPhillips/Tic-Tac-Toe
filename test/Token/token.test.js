1b4349ad486a40dddec96da0e55def015b7b934a
DeKayla Phillips <dphillips@8thlight.com>
2023-04-25 15:45:27 -0400

    Remove all helper function tests; constructor test passing with refactored constructor implementation

--- test/Token/token.test.js
+++ test/Token/token.test.js
@@ -7,54 +7,20 @@ describe('Token Class', () => {
             const token = new Token('X')
             const playerToken = token.player
             expect(playerToken).toBe('X')
-        });    
-    });
-
-    describe('isString()', () => {
-        test('will return a true boolean if a token is a string', () => {
-            const token1 = new Token('X')
-            const token2 = new Token('/.ABC')
-
-            const stringValidator1 = token1.isString()
-            const stringValidator2 = token2.isString()
-
-            expect(stringValidator1).toBeTruthy()
-            expect(stringValidator2).toBeTruthy()
-        });
-
-        test('will return a false boolean if a token is NOT a string', () => {
-            const token1 = new Token(1)
-            const token2 = new Token([])
-
-            const stringValidator1 = token1.isString()
-            const stringValidator2 = token2.isString()
-
-            expect(stringValidator1).toBeFalsy()
-            expect(stringValidator2).toBeFalsy()
         });  
-    });
-    
-    describe('isValidLength()', () => {
-        test('will return a true boolean if a token\'s length is equal to 1', () => {
-            const token1 = new Token('X');
-            const token2 = new Token('O');
 
-            const lengthValidator1 = token1.isValidLength();
-            const lengthValidator2 = token2.isValidLength();
+        test('will accept a token that is a string', () => {
 
-            expect(lengthValidator1).toBeTruthy();
-            expect(lengthValidator2).toBeTruthy();
+        }); 
+
+        test('will accept a token that is a length of 1', () => {
+
         });
-        
-        test('will return a false boolean if a token\'s length is NOT equal to 1', () => {
-            const token1 = new Token('XOX');
-            const token2 = new Token('ABC'); 
-            
-            const lengthValidator1 = token1.isValidLength();
-            const lengthValidator2 = token2.isValidLength();
 
-            expect(lengthValidator1).toBeFalsy();
-            expect(lengthValidator2).toBeFalsy();
+        test('will return a validated token', () => {
+
         });
     });
 });
+
+    
