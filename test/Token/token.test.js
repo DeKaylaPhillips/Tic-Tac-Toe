b8dfc538f565bde0896ca2fdd64906dbe53182bd
DeKayla Phillips <dphillips@8thlight.com>
2023-04-25 17:15:37 -0400

    Refactor token construction test; add comparison object to test

--- test/Token/token.test.js
+++ test/Token/token.test.js
@@ -4,13 +4,18 @@ describe('Token Class', () => {
 
     describe('Token constructor', () => {
         test('will accept a token that is a string', () => {
-            const token = new Token('X')
-            const tokenValue = token.player
-            expect(typeof tokenValue).toBe('string')
+            const token1 = new Token('X')
+            const token2 = new Token(1)
+
+            const tokenValue1 = token1.player
+            const tokenValue2 = token2.player
+
+            expect(typeof tokenValue1).toBe('string')
+            expect(typeof tokenValue2).not.toBe('string')
         });  
 
         test('will accept a token that is a length of 1', () => {
-            // test that the token has a length of 1
+                // test that the token has a length of 1
         });
 
         test('will return a validated token', () => {
