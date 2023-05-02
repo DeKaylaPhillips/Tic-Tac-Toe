3f9731d9a1357b4a2ce884f699756e69de96201f
DeKayla Phillips <dphillips@8thlight.com>
2023-05-02 10:57:46 -0400

    Add test for the getGameOverPrompt()

--- test/UserPrompts/userPrompts.test.js
+++ test/UserPrompts/userPrompts.test.js
@@ -9,8 +9,15 @@ describe('UserPrompts()', () => {
 
     describe('getWelcomeMessage()', () => {
         test('will return a string that welcomes the user to the game', () => {
-            const prompt = prompts.getWelcomeMessage()
+            const prompt = prompts.getWelcomePrompt()
             expect(prompt).toBe('\nWelcome to the classic game of Tic-Tac-Toe!\n');
+        }); 
+    }); 
+
+    describe('getGameOverPrompt()', () => {
+        test('will return a string informing the user the game is over', () => {
+            const prompt = prompts.getGameOverPrompt()
+            expect(prompt).toBe('\n|| --- GAME OVER! --- ||\n');
         }); 
     }); 
 });
