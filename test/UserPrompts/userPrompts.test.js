58d2b31f646afe60c66f6a371e2b0fb2f57c9216
DeKayla Phillips <dphillips@8thlight.com>
2023-05-02 16:46:39 -0400

    Add getPlaceTokenPrompt() and corresponding test for string return

--- test/UserPrompts/userPrompts.test.js
+++ test/UserPrompts/userPrompts.test.js
@@ -7,10 +7,17 @@ describe('UserPrompts()', () => {
         prompts = new UserPrompts();
     });
 
-    describe('getWelcomeMessage()', () => {
+    describe('getWelcomePrompt()', () => {
         test('will return a string that welcomes the user to the game', () => {
-            const prompt = prompts.getWelcomeMessage()
+            const prompt = prompts.getWelcomePrompt()
             expect(prompt).toBe('\nWelcome to the classic game of Tic-Tac-Toe!\n');
         }); 
-    }); 
+    });
+    
+    describe('getPlaceTokenPrompt()', () => {
+        test('will return a string informing the user to place a token on the board', () => {
+            const prompt = prompts.getPlaceTokenPrompt()
+            expect(prompt).toBe('\nPlease place a token on the board.\n');
+        }); 
+    });
 });
