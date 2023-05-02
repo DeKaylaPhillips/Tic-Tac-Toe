5adaed2ea165b19fefb695aa7fb012ad4be7719a
DeKayla Phillips <dphillips@8thlight.com>
2023-05-02 11:22:12 -0400

    Refactor getWelcomeMessage() describe block --> getWelcomePrompt()

--- test/UserPrompts/userPrompts.test.js
+++ test/UserPrompts/userPrompts.test.js
@@ -7,7 +7,7 @@ describe('UserPrompts()', () => {
         prompts = new UserPrompts();
     });
 
-    describe('getWelcomeMessage()', () => {
+    describe('getWelcomePrompt()', () => {
         test('will return a string that welcomes the user to the game', () => {
             const prompt = prompts.getWelcomePrompt()
             expect(prompt).toBe('\nWelcome to the classic game of Tic-Tac-Toe!\n');
