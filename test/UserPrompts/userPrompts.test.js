eba5fd6b94994c1468196aad69df226cbe7de456
DeKayla Phillips <dphillips@8thlight.com>
2023-05-03 13:24:55 -0400

    Display `Game Over` Message (#8)
    * Add getGameOverPrompt() method
    
    * Refactor call to the welcome message
    
    * Add test for the getGameOverPrompt()
    
    * Refactor getWelcomeMessage() describe block --> getWelcomePrompt()

--- test/UserPrompts/userPrompts.test.js
+++ test/UserPrompts/userPrompts.test.js
@@ -7,10 +7,17 @@ describe('UserPrompts()', () => {
         prompts = new UserPrompts();
     });
 
-    describe('getWelcomeMessage()', () => {
+    describe('getWelcomePrompt()', () => {
         test('will return a string that welcomes the user to the game', () => {
-            const prompt = prompts.getWelcomeMessage()
+            const prompt = prompts.getWelcomePrompt()
             expect(prompt).toBe('\nWelcome to the classic game of Tic-Tac-Toe!\n');
+        }); 
+    }); 
+
+    describe('getGameOverPrompt()', () => {
+        test('will return a string informing the user the game is over', () => {
+            const prompt = prompts.getGameOverPrompt()
+            expect(prompt).toBe('\n|| --- GAME OVER! --- ||\n');
         }); 
     }); 
 });
