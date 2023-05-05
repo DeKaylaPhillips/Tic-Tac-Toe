a5f9b88cf122499fc09db4e2a72bb280d12e3f64
DeKayla Phillips <dphillips@8thlight.com>
2023-05-05 12:48:23 -0400

    Save file

--- test/GameLogic/gameLogic.test.js
+++ test/GameLogic/gameLogic.test.js
@@ -12,20 +12,49 @@ describe('GameLogic', () => {
         board = new Board();
         playerToken = new Token('X').getToken();
         game = new GameLogic(board, playerToken);  
-        
-        board.placeToken(playerToken, 1);
-        board.placeToken(playerToken, 2);
     });
 
     describe('detectRowWin()', () => {
         test('will return a true boolean value when a row filled with the same token is detected', () => {
-            board.placeToken(playerToken, 3); // 'X' | 'X' | 'X' 
+            // X | X  | X 
+            // ''| '' | ''
+            // ''| '' | ''
+            board.placeToken(playerToken, 1);
+            board.placeToken(playerToken, 2);
+            board.placeToken(playerToken, 3);
+            const gameWinDetected = game.detectRowWin();
+            expect(gameWinDetected).toBeTruthy();
+        });
+
+        test('will return a true boolean value when a row filled with the same token is detected', () => {
+            // '' | '' | ''
+            // X  | X  | X
+            // '' | '' | ''
+            board.placeToken(playerToken, 4);
+            board.placeToken(playerToken, 5);
+            board.placeToken(playerToken, 6); 
+            const gameWinDetected = game.detectRowWin();
+            expect(gameWinDetected).toBeTruthy();
+        });
+        
+        test('will return a true boolean value when a row filled with the same token is detected', () => {
+            // '' | '' | ''
+            // '' | '' | ''
+            // X  | X  | X
+            board.placeToken(playerToken, 7);
+            board.placeToken(playerToken, 8);
+            board.placeToken(playerToken, 9); 
             const gameWinDetected = game.detectRowWin();
             expect(gameWinDetected).toBeTruthy();
         });
 
         test('will return a false boolean value when a row filled with the same token is not detected', () => {
-            board.placeToken(playerToken='O', 3); // 'X' | 'X' | 'O'
+            // X  | X  | O
+            // '' | '' | ''
+            // '' | '' | ''
+            board.placeToken(playerToken, 1);
+            board.placeToken(playerToken, 2);
+            board.placeToken(playerToken = 'O', 3); // 'X' | 'X' | 'O'
             const gameWinDetected = game.detectRowWin();
             expect(gameWinDetected).toBeFalsy(); 
         });
