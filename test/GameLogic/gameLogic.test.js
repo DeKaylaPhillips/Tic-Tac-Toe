c3291d6d7dc16220271001a60d62aa284b74b697
DeKayla Phillips <dphillips@8thlight.com>
2023-05-08 15:17:49 -0400

    Refactor detectRowWin() tests

--- test/GameLogic/gameLogic.test.js
+++ test/GameLogic/gameLogic.test.js
@@ -4,45 +4,35 @@ import { Token } from "../../lib/Token/token";
 
 describe('GameLogic', () => {
 
-    let playerToken;
-    let board;
-    let game;
-
-    beforeEach(() => {
-        board = new Board();
-        playerToken = new Token('X').getToken();
-        game = new GameLogic(board, playerToken);  
-    });
-
     describe('detectRowWin()', () => {
-        test('will return a true boolean value when a row filled with the same token is detected', () => {
-            let positions = [1, 2, 3]
-            positions.map((cell) => board.placeToken(playerToken, cell))
-            const gameWinDetected = game.detectRowWin();
-            expect(gameWinDetected).toBeTruthy();
-        });
-
-        test('will return a true boolean value when a row filled with the same token is detected', () => {
-            let positions = [4, 5, 6]
-            positions.map((cell) => board.placeToken(playerToken, cell))
-            const gameWinDetected = game.detectRowWin();
-            expect(gameWinDetected).toBeTruthy();
-        });
+        const playerToken = new Token('X').getToken();
+        const placeTokenFunc = (cell, board) => board.placeToken(playerToken, cell);
         
         test('will return a true boolean value when a row filled with the same token is detected', () => {
-            let positions = [7, 8, 9]
-            positions.map((cell) => board.placeToken(playerToken, cell))
-            const gameWinDetected = game.detectRowWin()
-            expect(gameWinDetected).toBeTruthy();
+            let positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+            
+            positions.map((row) => { 
+                let board = new Board();
+                let game = new GameLogic(board, playerToken);
+                row.map((col) => {
+                    placeTokenFunc(col, board); 
+                });
+
+                const gameWinDetected = game.detectRowWin();
+                expect(gameWinDetected).toEqual(true);
+            });
         });
 
         test('will return a false boolean value when a row filled with the same token is not detected', () => {
-            let positions = [1, 2, 3]
-            positions.map((cell, index) => (index + 1 !== positions[positions.length - 1]) ? 
-                board.placeToken(new Token('X').getToken(), cell) 
+            let board = new Board()
+            let game = new GameLogic(board, playerToken);
+            let positions = [1, 2, 3];
+            positions.map((cell, index) => (index + 1 !== positions[positions.length - 1]) ?
+                board.placeToken(playerToken, cell)
                 : board.placeToken(new Token('O').getToken(), cell));
+            
             const gameWinDetected = game.detectRowWin();
-            expect(gameWinDetected).toBeFalsy(); 
+            expect(gameWinDetected).toEqual(false);
         });
     });
 });
