be76107e1e9f622eade338b2f497ac68bd9aae3b
DeKayla Phillips <dphillips@8thlight.com>
2023-05-10 11:46:49 -0400

    Refactor test suite

--- test/GameLogic/gameLogic.test.js
+++ test/GameLogic/gameLogic.test.js
@@ -3,26 +3,26 @@ import { Board } from "../../lib/Board/board";
 import { Token } from "../../lib/Token/token";
 
 describe('GameLogic', () => {
-    describe('detectRowWin()', () => {
-        const board = new Board();
-        const playerToken = new Token('X').getToken();
-        const game = new GameLogic(board, playerToken); 
-        const placeTokenFunc = (cell, board) => board.placeToken(playerToken, cell);
-        
+    const playerToken = new Token('X').getToken();  
+    
+    describe('detectRowWin()', () => { 
         test('will return a true boolean value when a row filled with the same token is detected', () => {
             const positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-            positions.map((row) => { 
+            const placeTokenFunc = (cell, board) => board.placeToken(playerToken, cell);
+            positions.forEach((row) => { 
                 const board = new Board();
                 const game = new GameLogic(board, playerToken);
-                row.map((col) => placeTokenFunc(col, board));
+                row.forEach((col) => placeTokenFunc(col, board));
                 const winDetected = game.detectRowWin();
                 expect(winDetected).toEqual(true);
             });
         });
 
         test('will return a false boolean value when a row filled with the same token is not detected', () => {
+            const board = new Board();
+            const game = new GameLogic(board, playerToken);
             const positions = [1, 2, 3];
-            positions.map((cell) => !positions[positions.length - 1] ? 
+            positions.forEach((cell) => !positions[positions.length - 1] ? 
                 board.placeToken(playerToken, cell) : board.placeToken(new Token('O').getToken(), cell));    
             const winDetected = game.detectRowWin();
             expect(winDetected).toEqual(false);
@@ -30,23 +30,27 @@ describe('GameLogic', () => {
     });
 
     describe('getWinningToken()', () => {
-        const board = new Board();
-        const playerToken = new Token('X').getToken();
-        const game = new GameLogic(board, playerToken);
-        const placeTokenFunc = (cell) => board.placeToken(playerToken, cell);
+        let board;
+        let game;
         const positions = [1, 2, 3];
 
+        beforeEach(() => {
+            board = new Board();
+            game = new GameLogic(board, playerToken);
+        });
+        
         test('will return a string representation of the winning player\'s token', () => { 
-            positions.map(placeTokenFunc);
+            const placeTokenFunc = (cell) => board.placeToken(playerToken, cell);
+            positions.forEach(placeTokenFunc);
             const winner = game.getWinningToken();
             expect(winner).toEqual('X');
         });
 
         test('will return a null value if no winning player is found', () => {
-            positions.map((cell) => !positions[positions.length - 1] ? 
-                board.placeToken(playerToken, cell) : board.placeToken(playerToken, cell));
+            positions.forEach((cell) => !positions[positions.length - 1] ? 
+                board.placeToken(playerToken, cell) : board.placeToken(new Token('O').getToken(), cell));
             const winner = game.getWinningToken();
-            expect(winner).toEqual('X');
+            expect(winner).toEqual(null);
         });
     });
 });
