a92b77198b85a471ece0b928bb11066f8a32e5d6
DeKayla Phillips <dphillips@8thlight.com>
2023-05-10 11:54:01 -0400

    Detect Winner Token Based On Row Win Condition (#11)
    * Add method to return a winning token, and refactor methods to find and detect row wins
    
    * Add test to return the winning token and clean-up test suite; test currently in passing state
    
    * Refactor test description for test to return a string represention of the winning token
    
    * Refactor test suite

--- test/GameLogic/gameLogic.test.js
+++ test/GameLogic/gameLogic.test.js
@@ -3,36 +3,54 @@ import { Board } from "../../lib/Board/board";
 import { Token } from "../../lib/Token/token";
 
 describe('GameLogic', () => {
-
-    describe('detectRowWin()', () => {
-        const playerToken = new Token('X').getToken();
-        const placeTokenFunc = (cell, board) => board.placeToken(playerToken, cell);
-        
+    const playerToken = new Token('X').getToken();  
+    
+    describe('detectRowWin()', () => { 
         test('will return a true boolean value when a row filled with the same token is detected', () => {
-            let positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-            
-            positions.map((row) => { 
-                let board = new Board();
-                let game = new GameLogic(board, playerToken);
-                row.map((col) => {
-                    placeTokenFunc(col, board); 
-                });
-
-                const gameWinDetected = game.detectRowWin();
-                expect(gameWinDetected).toEqual(true);
+            const positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+            const placeTokenFunc = (cell, board) => board.placeToken(playerToken, cell);
+            positions.forEach((row) => { 
+                const board = new Board();
+                const game = new GameLogic(board, playerToken);
+                row.forEach((col) => placeTokenFunc(col, board));
+                const winDetected = game.detectRowWin();
+                expect(winDetected).toEqual(true);
             });
         });
 
         test('will return a false boolean value when a row filled with the same token is not detected', () => {
-            let board = new Board()
-            let game = new GameLogic(board, playerToken);
-            let positions = [1, 2, 3];
-            positions.map((cell, index) => (index + 1 !== positions[positions.length - 1]) ?
-                board.placeToken(playerToken, cell)
-                : board.placeToken(new Token('O').getToken(), cell));
-            
-            const gameWinDetected = game.detectRowWin();
-            expect(gameWinDetected).toEqual(false);
+            const board = new Board();
+            const game = new GameLogic(board, playerToken);
+            const positions = [1, 2, 3];
+            positions.forEach((cell) => !positions[positions.length - 1] ? 
+                board.placeToken(playerToken, cell) : board.placeToken(new Token('O').getToken(), cell));    
+            const winDetected = game.detectRowWin();
+            expect(winDetected).toEqual(false);
+        });
+    });
+
+    describe('getWinningToken()', () => {
+        let board;
+        let game;
+        const positions = [1, 2, 3];
+
+        beforeEach(() => {
+            board = new Board();
+            game = new GameLogic(board, playerToken);
+        });
+        
+        test('will return a string representation of the winning player\'s token', () => { 
+            const placeTokenFunc = (cell) => board.placeToken(playerToken, cell);
+            positions.forEach(placeTokenFunc);
+            const winner = game.getWinningToken();
+            expect(winner).toEqual('X');
+        });
+
+        test('will return a null value if no winning player is found', () => {
+            positions.forEach((cell) => !positions[positions.length - 1] ? 
+                board.placeToken(playerToken, cell) : board.placeToken(new Token('O').getToken(), cell));
+            const winner = game.getWinningToken();
+            expect(winner).toEqual(null);
         });
     });
 });
