0445e401ecca9db2662120675a990702d4c0f3ee
DeKayla Phillips <dphillips@8thlight.com>
2023-05-26 17:27:12 -0400

    Add method to detect if a board is filled

--- test/Board/board.test.js
+++ test/Board/board.test.js
@@ -31,17 +31,17 @@ describe('Board', () => {
 
     describe('placeToken()', () => {
         test('will place a valid token in the board when a valid position is accepted', () => {
-            board.placeToken(new Token('X'), 2);
+            const token = new Token('X')
+            board.placeToken(token, 2);
             const cells = board.getCells();
             expect(cells).toStrictEqual([['', 'X', ''], ['', '', ''], ['', '', '']]);
         });
     });
 
     describe('getColumnCells()', () => {
-        const token1 = new Token('X');
-        const token2 = new Token('?');
-
         test('returns a data structure containing tokens detected in each individual column', () => {
+            const token1 = new Token('X');
+            const token2 = new Token('?');
             const positions = [[1, 4, 7], [2, 5, 8], [3, 6, 9]];
             const placeTokenFn = (arr, num) => num != arr[arr.length - 1] ? board.placeToken(token1, num) : board.placeToken(token2, num);
             positions.forEach((col) => col.forEach((pos) => placeTokenFn(col, pos)))
@@ -51,10 +51,9 @@ describe('Board', () => {
     });
 
     describe('getMainDiagonalCells()', () => {
-        const token1 = new Token('X');
-        const token2 = new Token('Z');
-
         test('returns a data structure containing tokens detected in each main diagonal cell', () => {
+            const token1 = new Token('X');
+            const token2 = new Token('Z');
             const positions = [1, 5, 9];
             const placeTokenFn = (num) => num != positions[positions.length - 1] ? board.placeToken(token1, num) : board.placeToken(token2, num);
             positions.forEach((pos) => placeTokenFn(pos))
@@ -64,15 +63,27 @@ describe('Board', () => {
     });
 
     describe('getCounterDiagonalCells()', () => {
-        const token1 = new Token('?');
-        const token2 = new Token('S');
-
         test('returns a data structure containing tokens detected in each main diagonal cell', () => {
+            const token1 = new Token('?');
+            const token2 = new Token('S');
             const positions = [3, 5, 7];
             const placeTokenFn = (num) => num != positions[positions.length - 1] ? board.placeToken(token1, num) : board.placeToken(token2, num);
             positions.forEach((pos) => placeTokenFn(pos))
             const counterDiagonalCells = board.getCounterDiagonalCells();
             expect(counterDiagonalCells).toEqual(['?', '?', 'S']);
+        });
+    });
+
+    describe('detects full Tic-Tac-Toe board', () => {
+        test('will return a true boolean value when all spaces in the board are filled', () => {
+            const positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+            const token1 = new Token('X');
+            const token2 = new Token('O');
+            const placeTokenFunc = (cell) => (cell % 2 == 0 ? board.placeToken(token1, cell) : board.placeToken(token2, cell));
+            positions.forEach((row) => row.forEach((col) => placeTokenFunc(col)));
+            console.log(board.toString())
+            const fullBoardDetected = board.isFilledWithTokens()
+            expect(fullBoardDetected).toBe(true)
         });
     });
 });
